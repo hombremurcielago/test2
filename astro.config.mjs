2b128161f0e73e290318b3f2835b52a770e9879a
ldaniellicona01 <ldaniellicona01@gmail.com>
2024-08-26 20:12:59 +0000

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  site: 'https://github.com/hombremurcielago/test1.github.io.git',
+  base: 'my-repo',
+})
